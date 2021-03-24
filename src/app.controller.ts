c87c09f2baf87e09b42666e6bf0c38c607ef9c17
Bruno <brunomacedo_31@hotmail.com>
2021-03-24 17:29:30 -0300

    Circle cli

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,25 +1,24 @@
 import { Controller, Get, Res, HttpStatus, Query, Param } from '@nestjs/common';
 import { Response } from 'express';
 import { AppService } from './app.service';
-import * as MOCKED_RESPONSE from '../application.json'; // or use const inside the controller function
-
+import * as MOCKED_RESPONSE from '../application.json';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getPaymentMoethod(@Res() res: Response): any {
-    res.status(HttpStatus.OK).json(MOCKED_RESPONSE); // <= this sends response data as json
+  @Get('api')
+  getApplicationJson(@Res() res: Response): any {
+    res.status(HttpStatus.OK).json(MOCKED_RESPONSE); // retorna os dados do json
   }
 
   @Get('movies/:imgId')
   test(@Param('imgId') imgId, @Res() res) {
-    return res.sendFile(imgId, { root: 'public/movies' });
+    return res.sendFile(imgId, { root: 'public/movies' }); //retorna o cartaz do filme
   }
 
   @Get('chars/:imgId')
   test2(@Param('imgId') imgId, @Res() res) {
-    return res.sendFile(imgId, { root: 'public/chars' });
+    return res.sendFile(imgId, { root: 'public/chars' }); //retorna a imagem do personagem
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,14 @@ import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(
-    AppModule,
-  );
-  
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // app.setGlobalPrefix('api');
   app.use(compression());
   app.enableCors();
   app.useStaticAssets(join(__dirname, '..', 'images'));
-  app.use("/chars", express.static("images/chars"));
-  app.use("/movies", express.static("images/movies"));
+  app.use('chars', express.static('images/chars'));
+  app.use('movies', express.static('images/movies'));
   await app.listen(3000);
-
 }
 bootstrap();
