04235ae2f7acfee94ac02edd97342d7b4d175cfe
Bruno <brunomacedo_31@hotmail.com>
2021-03-24 18:31:28 -0300

    Add process.env.port

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'images'));
   app.use('chars', express.static('images/chars'));
   app.use('movies', express.static('images/movies'));
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
